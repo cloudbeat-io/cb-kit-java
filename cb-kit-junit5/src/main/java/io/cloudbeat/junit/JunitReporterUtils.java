10d5bb5f630e25f6eab8d7b420c8231e0a1e52c9
N. Dimer <ndimer@cloudbeat.io>
2024-09-19 20:58:48 +0300

    Properly report parameterized test methods.

--- cb-kit-junit5/src/main/java/io/cloudbeat/junit/JunitReporterUtils.java
+++ cb-kit-junit5/src/main/java/io/cloudbeat/junit/JunitReporterUtils.java
@@ -1,18 +1,20 @@
 package io.cloudbeat.junit;
 
-import io.cloudbeat.common.CbTestContext;
 import io.cloudbeat.common.reporter.CbTestReporter;
 import io.cloudbeat.common.reporter.model.CaseResult;
 import io.cloudbeat.common.reporter.model.StepResult;
+import io.cloudbeat.common.reporter.model.TestStatus;
 import io.cloudbeat.common.wrapper.console.SystemConsoleWrapper;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Optional;
 
 public class JunitReporterUtils {
     public static final String JAVA_METHOD_FQN_FORMAT = "%s#%s";
+    private static final ThreadLocal<HashMap<String, CaseResult>> caseResultByUniqueId = ThreadLocal.withInitial(HashMap::new);
     final static SystemConsoleWrapper consoleWrapper = new SystemConsoleWrapper();
 
     public static  void startInstance(CbTestReporter reporter, boolean wrapSystemConsole) {
@@ -30,40 +32,47 @@ public class JunitReporterUtils {
     public static void startSuite(CbTestReporter reporter, ExtensionContext context) {
         final String classFqn = context.getTestClass().get().getName();
         final String classDisplayName = context.getTestClass().get().getSimpleName();
-        //CbTestReporter reporter = CbTestContext.getReporter();
         reporter.startSuite(classDisplayName, classFqn);
         System.out.println("startSuite: " + context.getTestClass().get().getName());
     }
 
     public static void endSuite(CbTestReporter reporter, ExtensionContext context) {
         final String classFqn = context.getTestClass().get().getName();
-        //CbTestReporter reporter = CbTestContext.getReporter();
         reporter.endSuite(classFqn);
-        /*if (context.getStore(NAMESPACE).get(FAILED) == null) {
-            finishTestItem(context);
-        } else {
-            finishTestItem(context, FAILED);
-            context.getParent().ifPresent(p -> p.getStore(NAMESPACE).put(FAILED, Boolean.TRUE));
-        }*/
+        // Clear cached case results to free up some memory, as caching related to the suite
+        caseResultByUniqueId.get().clear();
     }
 
-    public static void startCase(CbTestReporter reporter, ExtensionContext context) throws Exception {
+    public static CaseResult startOrGetCase(CbTestReporter reporter, ExtensionContext context) throws Exception {
+        String uniqueId = context.getUniqueId();
+        if (caseResultByUniqueId.get().containsKey(uniqueId))
+            return caseResultByUniqueId.get().get(uniqueId);
         final String classFqn = context.getTestClass().get().getName();
         final String methodName = context.getTestMethod().get().getName();
         final String methodFqn = String.format(JAVA_METHOD_FQN_FORMAT, classFqn, methodName);
         CaseResult caseResult = reporter.startCase(methodName, methodFqn);
         caseResult.setDisplayName(context.getDisplayName());
+        caseResultByUniqueId.get().put(uniqueId, caseResult);
+        return caseResult;
     }
 
     public static void endCase(CbTestReporter reporter, ExtensionContext context) throws Exception {
+        String uniqueId = context.getUniqueId();
+        if (!caseResultByUniqueId.get().containsKey(uniqueId))
+            return;
+        CaseResult startedCase = caseResultByUniqueId.get().get(uniqueId);
+        /*
         final String classFqn = context.getTestClass().get().getName();
         final String methodName = context.getTestMethod().get().getName();
         final String methodFqn = String.format(JAVA_METHOD_FQN_FORMAT, classFqn, methodName);
+        */
         //CbTestReporter reporter = CbTestContext.getReporter();
         if (context.getExecutionException().isPresent())
-            reporter.failCase(methodFqn, context.getExecutionException().get());
+            reporter.endCase(startedCase, TestStatus.FAILED, context.getExecutionException().get());
+            // reporter.failCase(methodFqn, context.getExecutionException().get());
         else
-            reporter.endCase(methodFqn);
+            // reporter.endCase(methodFqn);
+            reporter.endCase(startedCase, null, null);
     }
 
     public static void disabledCase(CbTestReporter reporter, ExtensionContext context, Optional<String> reason) throws Exception {
@@ -111,10 +120,11 @@ public class JunitReporterUtils {
     public static void startCaseHook(CbTestReporter reporter, ExtensionContext context, boolean isBefore) {
         startCaseHook(reporter, context, isBefore,null);
     }
-    public static void startCaseHook(CbTestReporter reporter, String hookName, String hookFqn, boolean isBefore, Long startTime) {
+    public static StepResult startCaseHook(CbTestReporter reporter, String hookName, String hookFqn, boolean isBefore, Long startTime) {
         StepResult hookResult = reporter.startCaseHook(hookName, hookFqn, isBefore);
         if (startTime != null)
             hookResult.setStartTime(startTime);
+        return hookResult;
     }
     public static void startCaseHook(CbTestReporter reporter, ExtensionContext context, boolean isBefore, Long startTime) {
         final String classFqn = context.getTestClass().get().getName();
